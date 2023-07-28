0e674c2709fcc370195b5f0485182ceda1aa5fa5
Олександр <Pioner322622@gmail.com>
2023-07-28 14:08:37 +0300

    task 1

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+# task 1
+#
+# def multiply_list(lead_in):
+#     result = 1
+#     for num in lead_in:
+#         result *= num
+#     return result
+#
+#
+# random_list = []
+#
+# for i in range(10):
+#     random_list.append(random.randint(1, 11))
+#
+# print(f"Random List: {random_list}")
+# print(f"Product of elements in a list: {multiply_list(random_list)}")
+
