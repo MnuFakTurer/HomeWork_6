b937ea6d744151602f116089fcb4a5944d8d0f35
Олександр <Pioner322622@gmail.com>
2023-07-28 14:13:23 +0300

    task 3

--- main.py
+++ main.py
@@ -36,4 +36,30 @@ import random
 # print(f"Random List: {random_list}")
 # print(f"Product of elements in a list: {find_min(random_list)}")
 
+# task 3
+
+# def quantity_prime_number(num_list):
+#     def prime_number(num):
+#         if num < 2:
+#             return False
+#         for i in range(2, int(num ** 0.5) + 1):
+#             if num % i == 0:
+#                 return False
+#         return True
+#
+#     count = 0
+#     for num in num_list:
+#         if prime_number(num):
+#             count += 1
+#     return count
+#
+# random_list = []
+#
+# for i in range(10):
+#     random_list.append(random.randint(1, 100))
+#
+# num_primes = quantity_prime_number(random_list)
+#
+# print(f"Random List: {random_list}")
+# print(f"Product of elements in a list: {num_primes}")
 
